c0e6cc8170cd5e703e6c2eadd243159397c0cd80
AlexKlimenkov <shurick.klimenkov@gmail.com>
2018-02-27 18:36:40 +0300

    [fix] make sure gantt events are initialized only once per instance

--- src/components/Gantt.vue
+++ src/components/Gantt.vue
@@ -16,43 +16,51 @@ export default {
     }
   },
 
-  mounted () {
-
-    gantt.attachEvent('onTaskSelected', (id) => {
-      let task = gantt.getTask(id)
-      this.$emit('task-selected', task)
-    })
-
-    gantt.attachEvent('onAfterTaskAdd', (id, task) => {
-      this.$emit('task-updated', id, 'inserted', task)
-      task.progress = task.progress || 0
-      if(gantt.getSelectedId() == id) {
+  methods: {
+    $_initGanttEvents: function () {
+      if(gantt.$_eventsInitialized)
+        return;
+      gantt.attachEvent('onTaskSelected', (id) => {
+        let task = gantt.getTask(id)
         this.$emit('task-selected', task)
-      }
-    })
+      })
 
-    gantt.attachEvent('onAfterTaskUpdate', (id, task) => {
-      this.$emit('task-updated', id, 'updated', task)
-    })
+      gantt.attachEvent('onAfterTaskAdd', (id, task) => {
+        this.$emit('task-updated', id, 'inserted', task)
+        task.progress = task.progress || 0
+        if(gantt.getSelectedId() == id) {
+          this.$emit('task-selected', task)
+        }
+      })
 
-    gantt.attachEvent('onAfterTaskDelete', (id) => {
-      this.$emit('task-updated', id, 'deleted')
-      if(!gantt.getSelectedId()) {
-        this.$emit('task-selected', null)
-      }
-    })
+      gantt.attachEvent('onAfterTaskUpdate', (id, task) => {
+        this.$emit('task-updated', id, 'updated', task)
+      })
 
-    gantt.attachEvent('onAfterLinkAdd', (id, link) => {
-      this.$emit('link-updated', id, 'inserted', link)
-    })
+      gantt.attachEvent('onAfterTaskDelete', (id) => {
+        this.$emit('task-updated', id, 'deleted')
+        if(!gantt.getSelectedId()) {
+          this.$emit('task-selected', null)
+        }
+      })
 
-    gantt.attachEvent('onAfterLinkUpdate', (id, link) => {
-      this.$emit('link-updated', id, 'updated', link)
-    })
+      gantt.attachEvent('onAfterLinkAdd', (id, link) => {
+        this.$emit('link-updated', id, 'inserted', link)
+      })
 
-    gantt.attachEvent('onAfterLinkDelete', (id, link) => {
-      this.$emit('link-updated', id, 'deleted')
-    })
+      gantt.attachEvent('onAfterLinkUpdate', (id, link) => {
+        this.$emit('link-updated', id, 'updated', link)
+      })
+
+      gantt.attachEvent('onAfterLinkDelete', (id, link) => {
+        this.$emit('link-updated', id, 'deleted')
+      })
+      gantt.$_eventsInitialized = true;
+    }
+  },
+    
+  mounted () {
+    this.$_initGanttEvents();
 
     gantt.init(this.$refs.gantt)
     gantt.parse(this.$props.tasks)
@@ -61,5 +69,5 @@ export default {
 </script>
 
 <style>
-	@import "~dhtmlx-gantt/codebase/dhtmlxgantt.css";
+  @import "~dhtmlx-gantt/codebase/dhtmlxgantt.css";
 </style>
