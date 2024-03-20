3f1f587ac141af98764b012edf7e3d689de2bf08
Artyom Borisevich <aborisevich@xbsofware.by>
2024-03-20 13:05:53 +0300

    removed check statements

--- src/components/GanttComponent.vue
+++ src/components/GanttComponent.vue
@@ -14,46 +14,30 @@ export default {
       }
     }
   },
-  methods: {
-    $_initGanttEvents: function() {
-      if (!this.gantt.$_eventsInitialized) {
-        this.gantt.attachEvent('onTaskSelected', (id) => {
-          let task = this.gantt.getTask(id);
-          this.$emit('task-selected', task);
-        });
-
-        this.gantt.attachEvent('onTaskIdChange', (id, new_id) => {
-          if (this.gantt.getSelectedId() == new_id) {
-            let task = this.gantt.getTask(new_id);
-            this.$emit('task-selected', task);
-          }
-        });
-
-        this.gantt.$_eventsInitialized = true;
-      }
-    },
-
-    $_initDataProcessor: function() {
-      if (!this.gantt.$_dataProcessorInitialized) {
-        this.gantt.createDataProcessor((entity, action, data, id) => { 
-          this.$emit(`${entity}-updated`, id, action, data);
-        });
-
-        this.gantt.$_dataProcessorInitialized = true;
-      }
-    }
-  },
 
   mounted: function () {
     let gantt = Gantt.getGanttInstance();
-    this.gantt = gantt;
-    this.$_initGanttEvents();
     gantt.config.date_format = "%Y-%m-%d";
 
     gantt.init(this.$refs.ganttContainer);
     gantt.parse(this.$props.tasks);
 
-    this.$_initDataProcessor();
+    gantt.attachEvent('onTaskSelected', (id) => {
+      let task = gantt.getTask(id);
+      this.$emit('task-selected', task);
+    });
+
+    gantt.attachEvent('onTaskIdChange', (id, new_id) => {
+      if (gantt.getSelectedId() == new_id) {
+        let task = gantt.getTask(new_id);
+        this.$emit('task-selected', task);
+      }
+    });
+
+    gantt.createDataProcessor((entity, action, data, id) => { 
+      this.$emit(`${entity}-updated`, id, action, data);
+    });
+    
   },
 
   unmounted() {
